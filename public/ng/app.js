f4ad00b8fd1d1b311688e6a0a7b87756d8f26cad
原 高 <hoff_hoff@outlook.com>
2015-12-20 22:33:18 +0800

    angular

--- public/ng/app.js
+++ public/ng/app.js
@@ -5,16 +5,14 @@
 var sessionStorage = window.sessionStorage;
 
 // Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['ngRoute', 'ngResource', 'ngMessages', 'ngAnimate', 'ngTouch', 'cgBusy', 'ngMaterial']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngResource', 'ngMessages', 'ngAnimate', 'ngTouch', 'cgBusy']);
 myApp.config([
     '$routeProvider', 
     '$locationProvider',
     '$resourceProvider',
-    function($routeProvider, $locationProvider, $resourceProvider) {
+    function ($routeProvider, $locationProvider, $resourceProvider) {
         $routeProvider.when('/', { template: '', controller: '' });
         $routeProvider.when('/404', { templateUrl: ('partial/404'), controller: '' });
-        $routeProvider.when('/home', { templateUrl: ('partial/home'), controller: 'HomeCtrl' });
-        $routeProvider.when('/login', { templateUrl: ('partial/login'), controller: 'LoginCtrl' });        
         $routeProvider.when('/backDoor', { controller: 'LoginCtrl' });
         $routeProvider.otherwise({ redirectTo: '/404' });
         $locationProvider.html5Mode(true);
@@ -22,21 +20,21 @@ myApp.config([
         $resourceProvider.defaults.stripTrailingSlashes = false;// Don't strip trailing slashes from calculated URLs
     }
 ]);
-  
+
 myApp.run(['$location', 'SessionsService', function ($location, SessionsService) {
-    var path = $location.path();
-    if (path !== '/') {
-        $location.path('/');
-    } else {
-        path = '/home';
-    }
-    SessionsService.initSession(function(data){
-        $location.path(path);
-    }, function(err){
-        if (err.name == 'NeedSignIn') {
-            $location.path('/login');
+        var path = $location.path();
+        if (path !== '/') {
+            $location.path('/');
         } else {
-            alert(err.message);
+            path = '/home';
         }
-    });
-}]);
+        SessionsService.initSession(function (data) {
+            $location.path(path);
+        }, function (err) {
+            if (err.name == 'NeedSignIn') {
+                $location.path('/login');
+            } else {
+                alert(err.message);
+            }
+        });
+    }]);
