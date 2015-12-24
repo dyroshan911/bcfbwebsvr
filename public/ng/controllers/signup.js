30e003e7edccd82e16c8f5bbcde1febaedc5331f
原 高 <hoff_hoff@outlook.com>
2015-12-25 00:54:21 +0800

    login/signup

---
+++ public/ng/controllers/signup.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('myApp').controller('SignupCtrl', ['$scope', '$location', '$rootScope', 'UserService', 'SessionService', 
+	function ($scope, $location, $rootScope, UserService, SessionService) {
+		$scope.signupData = {
+			userName: '',
+			password: '',
+			passwordConfirm: '',
+			phone: '',
+			name: '',
+			email: '',
+			superior: ''
+		};
+		
+		$scope.onLogin = function () {
+			UserService.signup($rootScope.session.token, $scope.signupData, function (res) {
+				res;
+			}, function (res) {
+				res;
+			})
+		};
+	}]);
