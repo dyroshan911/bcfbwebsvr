14214b6744d74189d35cda6e006a6ae4b8d6474e
dyroshan911 <380534931>
2016-01-04 21:50:39 +0800

    update

--- controllers/customer.js
+++ controllers/customer.js
@@ -7,7 +7,7 @@ var users = require("../services/db").Users;
 exports.creatCustomer = function (token, customerObj, cb) {
     var result = {};
     var statusCode = 200;
-    sessions.getSessionAttrs(token, ['user_id', 'role'], function (err, data) {
+    sessions.getSessionAttrs(token, ['user_id', 'role','superior'], function (err, data) {
         if (!err && data.user_id) {
             if (data.role == 'member') {
                 customerObj.mem_id = data.user_id;
