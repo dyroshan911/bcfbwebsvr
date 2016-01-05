3751a5779592fe3faaed573fb4706eae4670fda7
原 高 <hoff_hoff@outlook.com>
2016-01-05 22:09:46 +0800

    add files

---
+++ public/ng/controllers/complete.js
@@ -0,0 +1,56 @@
+'use strict';
+
+angular.module('myApp').controller('CompleteCtrl', ['$scope', '$location', '$rootScope', 'UserService',
+	function ($scope, $location, $rootScope, UserService) {
+		$scope.accountData = {
+			userName: '',
+			password: ''
+		};
+		
+		//functions
+		$scope.onComplete = function () {
+			var dataObj = {
+				user_name: $scope.accountData.userName,
+				password: $scope.accountData.password
+			};
+			UserService.complete($rootScope.session.token, dataObj, function (res) {
+				alert(JSON.stringify(res));
+				UserService.login($rootScope.session.token, dataObj, function (res) {
+					alert(JSON.stringify(res));
+					$rootScope.session.logged = true;
+					$rootScope.session.userId = res.user_id;
+					$rootScope.session.userName = res.true_name;
+					$rootScope.session.role = res.role;
+					$scope.saveSessionData();
+					$location.path('/wechat-business');
+				}, function () {
+					alert(res.message);
+				});
+			}, function (res) {
+				alert(res.message);
+			})
+		};
+
+		$scope.onBind = function () {
+			var dataObj = {
+				user_name: $scope.accountData.userName,
+				password: $scope.accountData.password
+			};
+			UserService.bind($rootScope.session.token, dataObj, function (res) {
+				alert(JSON.stringify(res));
+				UserService.login($rootScope.session.token, dataObj, function (res) {
+					alert(JSON.stringify(res));
+					$rootScope.session.logged = true;
+					$rootScope.session.userId = res.user_id;
+					$rootScope.session.userName = res.true_name;
+					$rootScope.session.role = res.role;
+					$scope.saveSessionData();
+					$location.path('/wechat-business');
+				}, function () {
+					alert(res.message);
+				});
+			}, function (res) {
+				alert(res.message);
+			})
+		};
+	}]);
