5be1e836794e0bd227184208dd2c5d38bf6d91a5
原 高 <hoff_hoff@outlook.com>
2016-01-27 23:10:48 +0800

    review

--- backEndSvr/public/ng/controllers/home.js
+++ backEndSvr/public/ng/controllers/home.js
@@ -3,7 +3,7 @@
 angular.module('myApp').controller('HomeCtrl', ['$scope', '$location', '$routeParams',
 	function ($scope, $location, $routeParams) {
 		$scope.currentPage = $routeParams.params;
-		$scope.eachPageCount = 5;
+		$scope.eachPageCount = 15;
 
 		//functions
 		//pages
